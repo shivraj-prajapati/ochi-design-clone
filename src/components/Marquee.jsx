fa70fbbca0c3a5679a817e8120280d4a8023ec30
Shivraj Prajapati <rocky1989sp@gmail.com>
2024-11-04 10:30:08 +0530

    Adding animations

--- src/components/Marquee.jsx
+++ src/components/Marquee.jsx
@@ -3,24 +3,25 @@ import { motion } from "framer-motion";
 
 export default function Marquee() {
   return (
-    <div className="w-full py-10 bg-[#004D43] text-white rounded-tl-3xl rounded-tr-3xl">
-      <div className="text border-t-2 border-b-2 border-[#4C827B] flex whitespace-nowrap overflow-hidden gap-10">
+    <div data-scroll data-scroll-speed=".1" data-scroll-section className="w-full py-10 bg-[#004D43] text-white rounded-tl-3xl rounded-tr-3xl">
+      <div className="text border-t-2 border-b-2 border-[#4C827B] flex whitespace-nowrap overflow-hidden">
         <motion.h1
           initial={{ x: 0 }}
           animate={{ x: "-100%" }}
-          transition={{ ease: "linear", repeat: Infinity, duration: 5 }}
-          className="text-[17vw] leading-none font-HeadingPro font-semibold"
+          transition={{ ease: "linear", repeat: Infinity, duration: 18 }}
+          className="text-[17vw] leading-none font-HeadingPro font-semibold pr-20"
         >
-          We are ochi
+          We are ochi We are ochi
         </motion.h1>
         <motion.h1
           initial={{ x: 0 }}
           animate={{ x: "-100%" }}
-          transition={{ ease: "linear", repeat: Infinity, duration: 5 }}
-          className="text-[17vw] leading-none font-HeadingPro font-semibold"
+          transition={{ ease: "linear", repeat: Infinity, duration: 18 }}
+          className="text-[17vw] leading-none font-HeadingPro font-semibold pr-20"
         >
-          We are ochi
+          We are ochi We are ochi
         </motion.h1>
+        
       </div>
     </div>
   );
